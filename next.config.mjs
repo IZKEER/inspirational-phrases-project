fa21d802c332c87d6f8d93aaab18b3246cd3f08f
Marcelo Santos <97193948+IZKEER@users.noreply.github.com>
2025-03-18 22:38:42 +0000

    fix: github stuff not showing css

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,19 @@
 /** @type {import('next').NextConfig} */
+const repo = 'inspirational-phrases-project'; // Replace with your actual repository name
+
 const nextConfig = {
 	reactStrictMode: true,
 	output: 'export',
-	// output: {
-	// 	publicPath: "/",
-	// },
+	basePath: process.env.NODE_ENV === 'production' ? `/${repo}` : '',
+	assetPrefix: process.env.NODE_ENV === 'production' ? `/${repo}/` : '',
 	sassOptions: {
 		includePaths: ['node_modules'],
 		modules: true,
 	},
+	images: {
+		unoptimized: true, // Required for static export
+	},
+	trailingSlash: true, // Helps with GitHub Pages routing
 };
 
 export default nextConfig;
